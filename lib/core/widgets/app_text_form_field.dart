e43c02014c14375b9da551e5ae4e9a0a1547f857
MohamedFares09 <mofares010@gmail.com>
2025-07-31 22:18:16 +0300

    edit text field

--- lib/core/widgets/app_text_form_field.dart
+++ lib/core/widgets/app_text_form_field.dart
@@ -4,62 +4,72 @@ import 'package:your_health/core/theming/color.dart';
 
 // ignore: must_be_immutable
 class AppTextFormField extends StatelessWidget {
-  AppTextFormField(
-      {super.key,
-      required this.hintText,
-      required this.icon,
-      this.isObscureText,
-      this.prefixIcon,
-      this.controller,
-      this.validator});
+  AppTextFormField({
+    super.key,
+    required this.hintText,
+    required this.icon,
+    this.isObscureText,
+    this.prefixIcon,
+    this.controller,
+    this.validator,
+  });
+
   final String hintText;
-  Widget icon;
-  Widget? prefixIcon;
+  final Widget icon;
+  final Widget? prefixIcon;
   final bool? isObscureText;
-  TextEditingController? controller;
-  String? Function(String?)? validator;
+  final TextEditingController? controller;
+  final String? Function(String?)? validator;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 10.h),
       child: TextFormField(
-          validator: validator,
-          controller: controller,
-          textDirection: TextDirection.rtl,
-          textAlign: TextAlign.right,
-          decoration: InputDecoration(
-            isDense: true,
-            contentPadding:
-                EdgeInsets.symmetric(vertical: 12.h, horizontal: 18.w),
-            hintTextDirection: TextDirection.rtl,
-            hintText: hintText,
-            hintStyle: TextStyle(),
-            suffixIcon: icon,
-            prefixIcon: prefixIcon,
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(16.0),
-              borderSide: BorderSide(
-                color: Colors.grey,
-                width: 0.8,
-              ),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(16.0),
-              borderSide: BorderSide(
-                color: ColorManegaer.kprimarycolor,
-                width: 1.0,
-              ),
-            ),
-            errorBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(16.0),
-              borderSide: BorderSide(
-                color: Colors.red,
-                width: 1.0,
-              ),
+        controller: controller,
+        validator: validator,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        textDirection: TextDirection.rtl,
+        textAlign: TextAlign.right,
+        obscureText: isObscureText ?? false,
+        decoration: InputDecoration(
+          isDense: true,
+          contentPadding: EdgeInsets.symmetric(vertical: 12.h, horizontal: 18.w),
+          hintText: hintText,
+          hintTextDirection: TextDirection.rtl,
+          hintStyle: TextStyle(),
+          suffixIcon: icon,
+          prefixIcon: prefixIcon,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16.0),
+            borderSide: BorderSide(
+              color: Colors.grey,
+              width: 0.8,
             ),
           ),
-          obscureText: isObscureText ?? false),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16.0),
+            borderSide: BorderSide(
+              color: ColorManegaer.kprimarycolor,
+              width: 1.0,
+            ),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16.0),
+            borderSide: BorderSide(
+              color: Colors.red,
+              width: 1.0,
+            ),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16.0),
+            borderSide: BorderSide(
+              color: Colors.red,
+              width: 1.0,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
